85bec93c77427b2773a4adad6fd745c8c7714eb3
Sepehr <sepehr.ebi.1380@gmail.com>
2025-05-20 10:32:44 +0330

    Server socket on mobile network

--- mobile-network.cpp
+++ mobile-network.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 using namespace std;
 
@@ -9,5 +10,16 @@ int main(){
         printf("Couldn't create the server socket.\n");
         return 1;
     }
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8088);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+    if(listen(server_socket, 5) == -1){
+        printf("Couldn't listen in port 8088.\n");
+        return 2;
+    }
+    printf("Server is listening on port 8088.\n");
+    
     return 0;
 }
